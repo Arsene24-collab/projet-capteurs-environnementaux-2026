1627e849c534af1ad674ac6e65655070b9bc33a5
ketsiagraceb <ketsiagraceb@gmail.com>
2026-08-01 19:25:17 -0400

    Ajout des classes d'analyse des capteurs

---
+++ ketsiagraceb/analyse/AnalyseHumidite.java
@@ -0,0 +1,97 @@
+package analyse;
+
+import model.Mesure;
+import java.util.List;
+
+public class AnalyseHumidite {
+
+    public static double calculerMoyenne(List<Mesure> mesures) {
+
+        double somme = 0;
+        int compteur = 0;
+
+        for (Mesure mesure : mesures) {
+
+            if (mesure.getCapteur().equals("Humidité")) {
+
+                somme = somme + mesure.getValeur();
+                compteur++;
+            }
+        }
+
+        if (compteur == 0) {
+            return 0;
+        }
+
+        return somme / compteur;
+    }
+    public static double calculerMinimum(List<Mesure> mesures) {
+
+        double minimum = Double.MAX_VALUE;
+
+        for (Mesure mesure : mesures) {
+
+            if (mesure.getCapteur().equals("Humidité")) {
+
+                if (mesure.getValeur() < minimum) {
+                    minimum = mesure.getValeur();
+                }
+            }
+        }
+
+        return minimum;
+    }
+    public static double calculerMaximum(List<Mesure> mesures) {
+
+        double maximum = Double.MIN_VALUE;
+
+        for (Mesure mesure : mesures) {
+
+            if (mesure.getCapteur().equals("Humidité")) {
+
+                if (mesure.getValeur() > maximum) {
+                    maximum = mesure.getValeur();
+                }
+            }
+        }
+
+        return maximum;
+    }
+    public static double calculerVariance(List<Mesure> mesures) {
+
+        double moyenne = calculerMoyenne(mesures);
+        double somme = 0;
+        int compteur = 0;
+
+        for (Mesure mesure : mesures) {
+
+            if (mesure.getCapteur().equals("Humidité")) {
+
+                somme = somme + Math.pow(mesure.getValeur() - moyenne, 2);
+                compteur++;
+            }
+        }
+
+        if (compteur == 0) {
+            return 0;
+        }
+
+        return somme / compteur;
+    }
+    public static int compterAnomalies(List<Mesure> mesures) {
+
+        int compteur = 0;
+
+        for (Mesure mesure : mesures) {
+
+            if (mesure.getCapteur().equals("Humidité")) {
+
+                if (mesure.getValeur() < 30 || mesure.getValeur() > 90) {
+                    compteur++;
+                }
+            }
+        }
+
+        return compteur;
+    }
+}
